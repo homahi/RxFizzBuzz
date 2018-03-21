b8411bd19625bb9d85d16032fa002b7756f7915d
homahi <homahi0128@gmail.com>
2018-03-21 16:18:56 +0900

    RxFizzBuzzできるように作成

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxFizzBuzz' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'RxSwift'
+  pod 'RxCocoa'
+
+  # Pods for RxFizzBuzz
+
+  target 'RxFizzBuzzTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- RxFizzBuzz/ViewController.swift
+++ RxFizzBuzz/ViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var fizzBuzz: UILabel!
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var fizz: UILabel!
+    @IBOutlet weak var buzz: UILabel!
+    @IBOutlet weak var minus1: UIButton!
+    @IBOutlet weak var minus5: UIButton!
+    @IBOutlet weak var plus5: UIButton!
+    @IBOutlet weak var plus1: UIButton!
+    let fizzTrigger = BehaviorSubject<Bool>(value: true)
+    let buzzTrigger = BehaviorSubject<Bool>(value: true)
+    let fizzBuzzTrigger = BehaviorSubject<Bool>(value: true)
+    
+    let bag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        fizzTrigger.bind(to: fizz.rx.isHidden).disposed(by: bag)
+        buzzTrigger.bind(to: buzz.rx.isHidden).disposed(by: bag)
+        fizzBuzzTrigger.bind(to: fizzBuzz.rx.isHidden).disposed(by: bag)
+        
+        let counterObservable = Observable<Int>
+            .interval(1.0, scheduler: MainScheduler.instance)
+            .map{ _ in 1}
+            .startWith(0)
 
+        let plusOneObservable = plus1.rx.tap.map { _ in 1}
+        let plusFiveObservable = plus5.rx.tap.map { _ in 5}
+        let minusOneObservable = minus1.rx.tap.map { _ in -1}
+        let minusFiveObservable = minus5.rx.tap.map { _ in -5}
+        
+        let numberObservable = Observable
+            .merge(plusOneObservable,
+                   plusFiveObservable,
+                   minusOneObservable,
+                   minusFiveObservable,
+                   counterObservable)
+            .scan(0, accumulator: {$0 + $1})
+
+        numberObservable.map { String($0)}.bind(to: number.rx.text).disposed(by: bag)
+        
+        numberObservable
+            .skipWhile{ $0 == 0}
+            .map { $0 % 3 == 0 && $0 % 15 != 0 }
+            .debug("fizz")
+            .subscribe(onNext: { [weak self] bool in
+                self?.fizzTrigger.onNext(!bool)
+            }).disposed(by: bag)
+        
+        numberObservable
+            .skipWhile{$0 == 0}
+            .map { $0 % 5 == 0 && $0 % 15 != 0}
+            .debug("buzz")
+            .subscribe(onNext: { [weak self] bool in
+                self?.buzzTrigger.onNext(!bool)
+            }).disposed(by: bag)
+        
+        numberObservable
+            .skipWhile{$0 == 0}
+            .map { $0 % 15 == 0 }
+            .debug("fizzbuzz")
+            .subscribe(onNext: { [weak self] bool in
+                self?.fizzBuzzTrigger.onNext(!bool)
+            }).disposed(by: bag)
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
