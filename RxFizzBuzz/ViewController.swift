39f1f526e743dddeb42f050f49a40c12c7d41fa5
homahi <homahi0128@gmail.com>
2018-03-22 15:42:30 +0900

    FizzBuzz当ての機能を追加した。

--- RxFizzBuzz/ViewController.swift
+++ RxFizzBuzz/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+enum Type: String {
+    case Fizz = "Fizz"
+    case Buzz = "Buzz"
+    case FizzBuzz = "FizzBuzz"
+    case Other = "Other"
+    static func ==(lhs: Type, rhs: Type) -> Bool {
+        switch (lhs, rhs) {
+        case (.Fizz, .Fizz), (.Buzz, .Buzz), (.FizzBuzz, .FizzBuzz), (.Other, .Other):
+            return true
+        default:
+            return false
+        }
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var fizzBuzz: UILabel!
@@ -20,29 +35,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var minus5: UIButton!
     @IBOutlet weak var plus5: UIButton!
     @IBOutlet weak var plus1: UIButton!
-    let fizzTrigger = BehaviorSubject<Bool>(value: true)
-    let buzzTrigger = BehaviorSubject<Bool>(value: true)
-    let fizzBuzzTrigger = BehaviorSubject<Bool>(value: true)
+    @IBOutlet weak var fizzButton: UIButton!
+    @IBOutlet weak var buzzButton: UIButton!
+    @IBOutlet weak var fizzBuzzButton: UIButton!
+    @IBOutlet weak var answer: UILabel!
+    @IBOutlet weak var result: UILabel!
+    let publishAnswer = PublishSubject<String>()
     
     let bag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        fizzTrigger.bind(to: fizz.rx.isHidden).disposed(by: bag)
-        buzzTrigger.bind(to: buzz.rx.isHidden).disposed(by: bag)
-        fizzBuzzTrigger.bind(to: fizzBuzz.rx.isHidden).disposed(by: bag)
+        // 一定時間ごとに数値を流す
         
         let counterObservable = Observable<Int>
-            .interval(1.0, scheduler: MainScheduler.instance)
+            .interval(3.0, scheduler: MainScheduler.instance)
             .map{ _ in 1}
             .startWith(0)
-
+            .share()
+        
         let plusOneObservable = plus1.rx.tap.map { _ in 1}
         let plusFiveObservable = plus5.rx.tap.map { _ in 5}
         let minusOneObservable = minus1.rx.tap.map { _ in -1}
         let minusFiveObservable = minus5.rx.tap.map { _ in -5}
         
+        // 画面に表示されるカウント
         let numberObservable = Observable
             .merge(plusOneObservable,
                    plusFiveObservable,
@@ -50,34 +68,79 @@ class ViewController: UIViewController {
                    minusFiveObservable,
                    counterObservable)
             .scan(0, accumulator: {$0 + $1})
-
-        numberObservable.map { String($0)}.bind(to: number.rx.text).disposed(by: bag)
         
-        numberObservable
+        
+        // Fizzが表示されるタイミング
+        
+        let fizzObservable = numberObservable
             .skipWhile{ $0 == 0}
             .map { $0 % 3 == 0 && $0 % 15 != 0 }
-            .debug("fizz")
-            .subscribe(onNext: { [weak self] bool in
-                self?.fizzTrigger.onNext(!bool)
-            }).disposed(by: bag)
-        
-        numberObservable
+            .startWith(false)
+            .share()
+        let buzzObservable = numberObservable
             .skipWhile{$0 == 0}
             .map { $0 % 5 == 0 && $0 % 15 != 0}
-            .debug("buzz")
-            .subscribe(onNext: { [weak self] bool in
-                self?.buzzTrigger.onNext(!bool)
-            }).disposed(by: bag)
-        
-        numberObservable
+            .startWith(false)
+            .share()
+        let fizzBuzzObservable = numberObservable
             .skipWhile{$0 == 0}
             .map { $0 % 15 == 0 }
-            .debug("fizzbuzz")
-            .subscribe(onNext: { [weak self] bool in
-                self?.fizzBuzzTrigger.onNext(!bool)
-            }).disposed(by: bag)
+            .startWith(false)
+            .share()
         
+        let collectObservable: Observable<Type> = numberObservable.map{
+            if $0 % 3 == 0 && $0 % 15 != 0 {
+                return .Fizz
+            } else if $0 % 5 == 0 && $0 % 15 != 0 {
+                return .Buzz
+            } else if $0 % 15 == 0{
+                return .FizzBuzz
+            } else {
+                return .Other
+            }
+        }
         
+        let answerObservable = Observable<Type>
+            .merge(
+                fizzButton.rx.tap.map { _ in Type.Fizz },
+                buzzButton.rx.tap.map { _ in Type.Buzz},
+                fizzBuzzButton.rx.tap.map { _ in Type.FizzBuzz }
+            )
+            .sample(counterObservable)
+            .buffer(timeSpan: 3.0, count: 1, scheduler: MainScheduler.instance)
+            .map{ $0.count == 1 ? $0[0] : Type.Other }
+
+        let resultObservable = Observable<Bool>
+            .zip(collectObservable, answerObservable) { correct, answer in
+                return correct == answer
+        }.debug("result")
+        
+        fizzObservable
+            .map { !$0 }
+            .bind(to: fizz.rx.isHidden)
+            .disposed(by: bag)
+        buzzObservable
+            .map { !$0 }
+            .bind(to: buzz.rx.isHidden)
+            .disposed(by: bag)
+        fizzBuzzObservable
+            .map { !$0 }
+            .bind(to: fizzBuzz.rx.isHidden)
+            .disposed(by: bag)
+        numberObservable
+            .map { String($0)}
+            .bind(to: number.rx.text)
+            .disposed(by: bag)
+        answerObservable
+            .map {$0.rawValue}
+            .bind(to: answer.rx.text)
+            .disposed(by: bag)
+        resultObservable
+            .map { $0 ? "Correct!!": "Wrong" }
+            .startWith("")
+            .bind(to: result.rx.text)
+            .disposed(by: bag)
+
     }
     
     override func didReceiveMemoryWarning() {
